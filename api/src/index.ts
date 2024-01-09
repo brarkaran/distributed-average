560892aa68415f19ab2cd06dd0c230d16f51f5dd
Karan Brar <neoatom@neocore.local>
2024-01-09 11:57:32 -0800

    feat: implement file storage service on s3

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,5 +1,9 @@
 import express, { Request, Response } from 'express';
-import { KubeConfig, BatchV1Api, V1Job } from '@kubernetes/client-node';
+import { KubeConfig, BatchV1Api, V1Job, V1alpha1ValidatingAdmissionPolicyBindingSpec } from '@kubernetes/client-node';
+import fileRoutes from './routes/fileRoutes';
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config('../.env');
+}
 
 const app = express();
 app.use(express.json());
@@ -8,7 +12,7 @@ const kubeConfig = new KubeConfig();
 kubeConfig.loadFromDefault();
 const k8sApi = kubeConfig.makeApiClient(BatchV1Api);
 
-app.post('/create-job', async (req: Request, res: Response) => {
+app.post('api/workers/', async (req: Request, res: Response) => {
     const numWorkers = req.body.numWorkers || 1;
     const jobName = `worker-job-${Date.now()}`;
 
@@ -42,18 +46,8 @@ app.post('/create-job', async (req: Request, res: Response) => {
     }
 });
 
-app.post('/submit-job', async (req: Request, res: Response) => {
-    const numWorkers = req.body.numWorkers || 1;
-    const { files, count } = req.body;
 
-    try {
-        await k8sApi.createNamespacedJob('default', jobManifest); // Replace 'default' with your namespace if different
-        res.status(200).json({ message: `Job ${jobName} created with ${numWorkers} workers` });
-    } catch (error) {
-        console.error("Error creating Kubernetes job:", error);
-        res.status(500).json({ error: "Failed to create job on Kubernetes cluster" });
-    }
-})
+app.use(fileRoutes);
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
